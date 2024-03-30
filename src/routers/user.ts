7823b2e08bcdf32248d0826d8a63fa0510933147
PaviterSingh <paviter.singh@testingxperts.com>
2024-03-30 13:53:30 +0530

    new login route and formate

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,24 +1,37 @@
 import express, { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
-import { userJSON } from '../types/user';
+import { postLoginBody, postUserBody, userJSON } from '../types/user';
 
-const router =  express.Router()
+const router = express.Router()
 
-router.post('/user', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/user', async (req: Request<{}, {}, postUserBody>, res: Response, next: NextFunction) => {
     const user = new User(req.body)
     try {
         await user.save()
-        
+
         const token = await user.generateAuthToken()
         const userInfo: userJSON = await user.toJSON()
-        
-        res.status(201).send({ userInfo, token })
+
+        res.status(201).send({ user: userInfo, token })
 
     } catch (e: any) {
-        if(e.code == 11000){
+        if (e.code === 11000) {
             return res.status(400).send("email exist")
         }
         res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req: Request<{}, {}, postLoginBody>, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findByCredentials(email, password)
+        const token = await user.generateAuthToken();
+        const userInfo = await user.toJSON()
+        res.send({ user: userInfo, token })
+    }
+    catch (e) {
+        res.status(400).send(e);
     }
 })
 
